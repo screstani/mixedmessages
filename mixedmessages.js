d8f408e1c3110da0981a308aab5616dbbf830eaa
Sayonara Crestani <sayonara.crestani@gmail.com>
2022-08-13 12:39:59 -0300

    "put all the arrays into one object"

--- mixedmessages.js
+++ mixedmessages.js
@@ -1,12 +1,10 @@
-const celestialBodies = ["Moon", "Sun", "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"]
-
-const currentState = ["annoyed", "sorrowful", "uncertain", "pleased", "empowered", "vibrant"]
-
-const pieceOfAdvice = ["trust in yourself", "don't wear yellow", "be more grateful", "mind the gap", "be careful"]
-
-let randomCelestialBody = celestialBodies[Math.floor(Math.random() * celestialBodies.length)];
-let randomCurrentState = currentState[Math.floor(Math.random() * currentState.length)];
-let randomPieceOfAdvice = pieceOfAdvice[Math.floor(Math.random() * pieceOfAdvice.length)];
+const messages = {celestialBodies: ["Moon", "Sun", "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"],
+                  currentState: ["annoyed", "sorrowful", "uncertain", "pleased", "empowered", "vibrant"],
+                  pieceOfAdvice: ["trust in yourself", "don't wear yellow", "be more grateful", "mind the gap", "be careful"]
+}
+let randomCelestialBody = messages.celestialBodies[Math.floor(Math.random() * messages.celestialBodies.length)];
+let randomCurrentState = messages.currentState[Math.floor(Math.random() * messages.currentState.length)];
+let randomPieceOfAdvice = messages.pieceOfAdvice[Math.floor(Math.random() * messages.pieceOfAdvice.length)];
 
 const wordOfTheDay = () => `Today your celestial guidance body is ${randomCelestialBody}, that means you might feel a little ${randomCurrentState}. 
 My advice? ${randomPieceOfAdvice}!`
